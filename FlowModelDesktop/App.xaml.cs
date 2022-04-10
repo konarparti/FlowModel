8a57b37fb1dfcbcf8354b0bde46e8c428cb2daf4
Takandrew <andreikaril@mail.ru>
2022-04-10 21:58:46 +0300

    Add MVVM

--- FlowModelDesktop/App.xaml.cs
+++ FlowModelDesktop/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Autofac;
+using FlowModelDesktop.ViewModel;
 
 namespace FlowModelDesktop
 {
@@ -13,5 +15,16 @@ namespace FlowModelDesktop
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<MainWindowViewModel>().AsSelf();
+            var container = builder.Build();
+            var mainWindowViewModel = container.Resolve<MainWindowViewModel>();
+            var mainWindow = new MainWindow { DataContext = mainWindowViewModel };
+            mainWindow.Show();
+        }
+         
     }
 }
---
+++ FlowModelDesktop/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FlowModelDesktop.Models;
+using WPF_MVVM_Classes;
+
+namespace FlowModelDesktop.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private InputData _inputData;
+        private DbData _dbData;
+
+        public InputData InputData
+        {
+            get => _inputData;
+            set
+            {
+                _inputData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DbData DbData
+        {
+            get => _dbData;
+            set
+            {
+                _dbData = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private RelayCommand? _calculateCommand;
+
+        public RelayCommand CalculateCommand
+        {
+            get
+            {
+                return _calculateCommand ??= new RelayCommand(x =>
+                {
+                    MessageBox.Show(InputData.L.ToString());
+                });
+            }
+        }
+    }
+}
