98f0885272f59001eaadead609e28e2d126fa327
Anton <anton.gusevv.01@gmail.com>
2022-04-11 18:08:28 +0300

    add opening of child window with mvvm

---
+++ FlowModelDesktop/Services/ViewModelBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using FlowModelDesktop.ViewModel;
+
+namespace FlowModelDesktop.Services
+{
+    public abstract class ViewModelBase : DependencyObject, INotifyPropertyChanged, IDisposable
+    {
+        public void Dispose()
+        {
+            OnDispose();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+
+        protected virtual void OnDispose()
+        {
+        }
+
+        ///
+        /// Окно в котором показывается текущий ViewModel
+        ///
+        private ChartsWindow _window = null;
+
+        protected virtual void Closed()
+        {
+
+        }
+
+        ///
+        /// Методы вызываемый для закрытия окна связанного с ViewModel
+        ///
+        public bool Close()
+        {
+            var result = false;
+            if (_window != null)
+            {
+                _window.Close();
+                _window = null;
+                result = true;
+            }
+            return result;
+        }
+
+        ///
+        /// Метод показа ViewModel в окне
+        ///
+        /// viewModel">
+        protected void Show(ViewModelBase viewModel)
+        {
+            viewModel._window = new ChartsWindow
+            {
+                DataContext = viewModel
+            };
+            viewModel._window.Closed += (sender, e) => Closed();
+            viewModel._window.Show();
+        }
+    }
+}
--- FlowModelDesktop/ViewModel/MainWindowViewModel.cs
+++ FlowModelDesktop/ViewModel/MainWindowViewModel.cs
@@ -5,8 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using FlowModelDesktop.Models;
+using FlowModelDesktop.Services;
 using WPF_MVVM_Classes;
 using Math = FlowModelDesktop.Models.Math;
+using ViewModelBase = FlowModelDesktop.Services.ViewModelBase;
 
 namespace FlowModelDesktop.ViewModel
 {
@@ -14,6 +16,9 @@ namespace FlowModelDesktop.ViewModel
     {
         private InputData _inputData = new InputData();
         private DbData _dbData = new DbData();
+        private RelayCommand? _calculateCommand;
+        private RelayCommand? _openChartsCommand;
+
 
         public InputData InputData
         {
@@ -35,7 +40,6 @@ namespace FlowModelDesktop.ViewModel
             }
         }
 
-        private RelayCommand? _calculateCommand;
 
         public RelayCommand CalculateCommand
         {
@@ -46,6 +50,18 @@ namespace FlowModelDesktop.ViewModel
                     var math = new Math();
                     math.Calculation(InputData, DbData, out decimal Q, out decimal Tp, out decimal Etap);
                     MessageBox.Show($"Q = {System.Math.Round(Q,2)}\nTp = {System.Math.Round(Tp,2)}\nEtap = {System.Math.Round(Etap,2)}");
+                });
+            }
+        }
+
+        public RelayCommand OpenChartsCommand
+        {
+            get
+            {
+                return _openChartsCommand ??= new RelayCommand(x =>
+                {
+                    var child = new ChartsWindowViewModel(1);
+                    Show(child);
                 });
             }
         }
