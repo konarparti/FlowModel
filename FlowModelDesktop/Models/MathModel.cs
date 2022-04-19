4dc3a2f78a00438783c2b678702e8cd1f50654c4
Anton <anton.gusevv.01@gmail.com>
2022-04-19 19:44:13 +0300

    small fix in math model

--- FlowModelDesktop/Models/MathModel.cs
+++ FlowModelDesktop/Models/MathModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace FlowModelDesktop.Models
 {
@@ -15,27 +16,18 @@ namespace FlowModelDesktop.Models
             //_dbData = dbData;
         }
 
-        public static decimal DecimalPow(decimal x, decimal y)
-        {
-            return (decimal)System.Math.Pow(decimal.ToDouble(x), decimal.ToDouble(y));
-        }
-
-        public static decimal DecimalLn(decimal x)
-        {
-            return (decimal)System.Math.Log(decimal.ToDouble(x));
-        }
 
         public void Calculation(InputData inputData, DbData dbData, out decimal Q, out List<decimal> Tp_List, out List<decimal> Eta_List, out TimeSpan time, out long memory)
         {
             var stopwatch = new Stopwatch();
             var process = Process.GetCurrentProcess();
             stopwatch.Start();
-            decimal F = 0.125m * DecimalPow((inputData.H / inputData.W), 2) - 0.625m * (inputData.H / inputData.W) + 1;
-            decimal Qch = (inputData.H * inputData.W * inputData.Vu * F) / 2;
+            decimal F = 0.125m * DecimalMath.PowerN((inputData.H / inputData.W), 2) - 0.625m * (inputData.H / inputData.W) + 1;
+            decimal Qch = inputData.H * inputData.W * inputData.Vu / 2 * F;
             decimal gamma = inputData.Vu / inputData.H;
-            decimal q_gamma = inputData.H * inputData.W * dbData.Mu * DecimalPow(gamma, (dbData.n + 1));
-            decimal q_alpha = inputData.W * dbData.alpha_u * (DecimalPow(dbData.b, (-1)) - inputData.Tu - dbData.Tr);
-            decimal N = inputData.L / inputData.DeltaZ;
+            decimal q_gamma = inputData.H * inputData.W * dbData.Mu * DecimalMath.Power(gamma, (dbData.n + 1));
+            decimal q_alpha = inputData.W * dbData.alpha_u * ((1/dbData.b) - inputData.Tu + dbData.Tr);
+            decimal N = Math.Round(inputData.L / inputData.DeltaZ);
             
             Tp_List = new List<decimal>();
             Eta_List = new List<decimal>();
@@ -43,20 +35,19 @@ namespace FlowModelDesktop.Models
             for (int i = 0; i <= N; i++)
             {
                 z = i * inputData.DeltaZ;
-                
                 {
-                    T = dbData.Tr + (1 / dbData.b) * DecimalLn(
+                    T = dbData.Tr + (1 / dbData.b) * DecimalMath.Log(
                         ((dbData.b * q_gamma + inputData.W * dbData.alpha_u) / (dbData.b * q_alpha))
-                        * (1 - DecimalPow((decimal) System.Math.E, ((-dbData.b * q_alpha * z) / (dbData.ro * dbData.c * Qch))))
-                        + DecimalPow((decimal) System.Math.E, (dbData.b * (dbData.To - dbData.Tr - ((q_alpha * z) / (dbData.ro * dbData.c * Qch))))));
+                        * (1 - DecimalMath.Power(DecimalMath.E, ((-dbData.b * q_alpha * z) / (dbData.ro * dbData.c * Qch))))
+                        + DecimalMath.Power(DecimalMath.E, (dbData.b * (dbData.To - dbData.Tr - ((q_alpha * z) / (dbData.ro * dbData.c * Qch))))));
                     Tp_List.Add(System.Math.Round(T, 2));
-                    Eta_List.Add(System.Math.Round(dbData.Mu * DecimalPow((decimal) System.Math.E, (-dbData.b * (T - dbData.Tr))) * DecimalPow(gamma, (dbData.n - 1)), 2));
+                    Eta_List.Add(System.Math.Round(dbData.Mu * DecimalMath.Power(DecimalMath.E, (-dbData.b * (T - dbData.Tr))) * DecimalMath.Power(gamma, (dbData.n - 1)), 2));
                 }
             }
 
             Q = dbData.ro * Qch;
-            decimal Tp = Tp_List[Tp_List.Count-1];
-            decimal Etap = Eta_List[Eta_List.Count-1];
+            decimal Tp = Tp_List.Last();
+            decimal Etap = Eta_List.Last();
             stopwatch.Stop();
             time = stopwatch.Elapsed;
             memory = process.PagedMemorySize64;
